1a4e69c54edcd12e4a3b83149543fc2ea5cc5687
jobsons10 <jobsonsobrinho@grupootg.com>
2022-12-07 14:53:03 -0400

    hide-me

---
+++ script.js
@@ -0,0 +1,7 @@
+const ShowMeTwo = function() {
+    document.getElementById('two').classList.remove('hide-me');
+}
+
+const ShowMeThree = function() {
+    document.getElementById('three').classList.remove('hide-me');
+}
