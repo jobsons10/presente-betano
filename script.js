354510658f5967e9d0e816e89a4eb28993102536
jobsons10 <jobsonsobrinho@grupootg.com>
2022-12-07 16:17:12 -0400

    a

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const ShowMeTwo = function() {
     document.getElementById('two').classList.remove('hide-me');
+    document.getElementById('one').classList.add('hide-me');
 }
 
 const ShowMeThree = function() {
+    document.getElementById('two').classList.add('hide-me');
     document.getElementById('three').classList.remove('hide-me');
 }
